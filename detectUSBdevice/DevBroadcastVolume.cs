8850a8985bb63fdd9dd2088b6a50aa029e21fbdd
Administrator <Administrator@TPER40009328>
2016-08-10 16:37:23 +0800

    加入專案檔案。

---
+++ detectUSBdevice/DevBroadcastVolume.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace detectUSBdevice
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public class DevBroadcastVolume
+    {
+        public int Size;
+        public int DeviceType;
+        public int Reserved;
+        public int Mask;
+        public Int16 Flags;
+    }
+}
---
+++ detectUSBdevice/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace detectUSBdevice
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_DEVICECHANGE = 0x219;
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        private const int DBT_DEVTYP_VOLUME = 0x00000002;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            switch (m.Msg)
+            {                
+                case WM_DEVICECHANGE:
+                    switch ((int)m.WParam)
+                    {
+                        case DBT_DEVICEARRIVAL:
+                            listBox1.Items.Add("New Device Arrived");
+                            int devType = Marshal.ReadInt32(m.LParam, 4);
+                            if (devType == DBT_DEVTYP_VOLUME)
+                            {
+                                DevBroadcastVolume vol;
+                                vol = (DevBroadcastVolume)Marshal.PtrToStructure(m.LParam,
+                                   typeof(DevBroadcastVolume));
+                                listBox1.Items.Add("Mask is " + vol.Mask);
+                            }
+                            break;
+
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            listBox1.Items.Add("Device Removed");
+                            break;
+                    }
+                    break;
+            }
+        }
+
+    }
+}
